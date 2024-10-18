16ebd00e3f89d239e2e3786ae3cf1ecf87eb137b
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-10-18 12:27:27 +0530

    CustomBeanNaming, RetrieveBean(name,type), CreatingAndAutowiringBeans(methodCall,Parameters)

--- LearningSpringConcepts/src/main/java/com/SpringFeatures/LearningSpringConcepts/App02HelloWorldSpring.java
+++ LearningSpringConcepts/src/main/java/com/SpringFeatures/LearningSpringConcepts/App02HelloWorldSpring.java
@@ -8,8 +8,21 @@ public class App02HelloWorldSpring {
         //1: Launch a Spring Context
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
-        //3: Retrieving the Beans managed by the Spring
+        //3: Retrieving the Beans managed by the Spring with the help of it's name
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+//        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("myAddress"));
+
+        // Retrieving the Bean with the help of it's type
+//        System.out.println(context.getBean(Address.class));
+
+        // Retrieving the Bean which is Autowired with other Beans using Method Call
+        System.out.println(context.getBean("person2MethodCall"));
+
+        // Retrieving the Bean which is Autowired with other Beans using existing Beans as Parameters
+        System.out.println(context.getBean("person3Parameters"));
 
     }
 }
--- LearningSpringConcepts/src/main/java/com/SpringFeatures/LearningSpringConcepts/HelloWorldConfiguration.java
+++ LearningSpringConcepts/src/main/java/com/SpringFeatures/LearningSpringConcepts/HelloWorldConfiguration.java
@@ -4,12 +4,50 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+record Person(String name, int age, Address address) {
+}
+
+record Address(String firstLine, String city) {
+}
+
 @Configuration
 //2: Configure the things that we want Spring to manage- @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
-    public String name(){
+    public String name() {
         return "Kartik Jaiswal";
+    }
+
+    @Bean
+    public int age() {
+        return 22;
+    }
+
+    @Bean
+    public Person person() {
+        var person = new Person("Kartik Jaiswal", 22, new Address("Babron", "Kurawar"));
+        System.out.println(person.toString());
+        return person;
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());  // Autowiring the Bean using Method Call
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address friendAddress) {
+        return new Person(name, age, friendAddress);  // Autowiring the Bean using existing Beans as Parameters
+    }
+
+    @Bean(name = "myAddress")
+    public Address address() {
+        return new Address("Babron", "Kurawar");
+    }
+
+    @Bean(name = "friendAddress")
+    public Address addressFriend() {
+        return new Address("NariyalKheda", "Bhopal");
     }
 }
