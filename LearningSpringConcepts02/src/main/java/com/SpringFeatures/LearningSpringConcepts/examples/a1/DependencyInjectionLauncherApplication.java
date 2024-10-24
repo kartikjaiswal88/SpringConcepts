e2171983f1409dbfd8d42894cb1faa549cd7e21a
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-10-24 19:38:00 +0530

    Field Injection

--- LearningSpringConcepts02/src/main/java/com/SpringFeatures/LearningSpringConcepts/examples/a1/DependencyInjectionLauncherApplication.java
+++ LearningSpringConcepts02/src/main/java/com/SpringFeatures/LearningSpringConcepts/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,10 +1,33 @@
 package com.SpringFeatures.LearningSpringConcepts.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+
+@Component
+class YourBusinessClass{
+    @Autowired
+    Dependency1 dependency1;
+    @Autowired
+    Dependency2 dependency2;
+    @Override
+    public String toString(){
+        return "Using==>  " + dependency1 + " and " + dependency2;
+    }
+
+}
+@Component
+class Dependency1{
+
+}
+@Component
+class Dependency2{
+
+}
 
 @Configuration
 @ComponentScan // Automatically scan the current package for DI
@@ -15,7 +38,10 @@ class DependencyInjectionLauncherApplication {
         try (var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
 
 //            System.out.println(context.getBeanDefinitionNames());
-            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+//            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            var yourBusinessClass = context.getBean(YourBusinessClass.class);
+            System.out.println(yourBusinessClass);
         }
     }
 }
